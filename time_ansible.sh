d8dad7485755fb542d4303682356a033a0f9956d
George-Papoutsis-123 <georgepapoutsis@cmail.carleton.ca>
2026-03-28 14:28:40 -0400

    Script for timing the ansible deployment

---
+++ time_ansible.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+PLAYBOOK="DeployOAI.yaml"
+
+START=$(date +%s%3N)
+
+ansible-playbook -i "localhost," -c local "$PLAYBOOK" --become
+
+END=$(date +%s%3N)
+
+ELAPSED_MS=$((END - START))
+
+echo "Ansible deployment time: ${ELAPSED_MS} ms"
