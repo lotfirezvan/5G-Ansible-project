059a0b8855f2d29bac3faee8088774698474c095
lotfirezvan <lotfi.rezvan@gmail.com>
2026-04-08 22:37:14 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,6 @@
 # Ping test
+# Test connectivity of OAI 5G network
+
 docker exec oai-ext-dn ping -c 3 12.1.1.2 #Ping the UE from the external DN to verify connectivity. You should see successful ping responses if everything is set up correctly.
 docker exec gnbsim ping -c 3 -I 12.1.1.2 google.com #Ping an external address from the UE to verify internet connectivity. You should see successful ping responses if everything is set up correctly.
 
